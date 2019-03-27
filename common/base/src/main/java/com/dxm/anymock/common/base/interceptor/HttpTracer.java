861840431899fbbdb95a01d043dbb258e43126b7
lizhenxi <anymock@duxiaoman.com>
2019-03-28 03:49:04 +0800

    ignore / access

--- common/base/src/main/java/com/dxm/anymock/common/base/interceptor/HttpTracer.java
+++ common/base/src/main/java/com/dxm/anymock/common/base/interceptor/HttpTracer.java
@@ -1,5 +1,6 @@
 package com.dxm.anymock.common.base.interceptor;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -13,7 +14,9 @@ public class HttpTracer extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        logger.info("{} {} {}", request.getRemoteAddr(), request.getMethod(), request.getRequestURI());
+        if (!StringUtils.equals(request.getRequestURI(), "/")) {
+            logger.info("{} {} {}", request.getRemoteAddr(), request.getMethod(), request.getRequestURI());
+        }
         return true;
     }
 }
--- core/web/src/main/java/com/dxm/anymock/core/web/controller/HttpController.java
+++ core/web/src/main/java/com/dxm/anymock/core/web/controller/HttpController.java
@@ -4,6 +4,7 @@ import com.dxm.anymock.core.biz.service.HttpMockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,10 @@ public class HttpController {
 
     @Autowired
     private HttpMockService httpMockService;
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public void ignore() {}
 
     @RequestMapping(value = "/**/*")
     public void handleHttpRequests(HttpServletRequest request, HttpServletResponse response) throws IOException {
