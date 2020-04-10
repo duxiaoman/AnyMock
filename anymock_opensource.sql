a7736c63f2afbd02c089fca70299fe309c129444
ReneJiang <jiangfanfan1523@126.com>
2020-04-10 13:12:59 +0800

    Update anymock_opensource.sql
    add tcp interface ddl

--- anymock_opensource.sql
+++ anymock_opensource.sql
@@ -50,12 +50,12 @@ CREATE TABLE `anymock_http_interface`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for anymock_http_interface_branch
+-- Table structure for anymock_interface_branch
 -- ----------------------------
-DROP TABLE IF EXISTS `anymock_http_interface_branch`;
-CREATE TABLE `anymock_http_interface_branch`  (
+DROP TABLE IF EXISTS `anymock_interface_branch`;
+CREATE TABLE `anymock_interface_branch`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `http_interface_id` bigint(20) NOT NULL,
+  `interface_id` bigint(20) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sync_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `async_script` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -116,3 +116,46 @@ CREATE TABLE `anymock_space`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 91 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+CREATE TABLE `anymock_tcp_interface` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `request_uri` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `need_async_callback` tinyint(1) NOT NULL,
+  `config_mode` varchar(255) NOT NULL,
+  `response_body` mediumtext NOT NULL,
+  `callback_request_url` varchar(255) NOT NULL,
+  `callback_request_body` mediumtext NOT NULL,
+  `branch_jump_script` mediumtext NOT NULL,
+  `sync_script` mediumtext NOT NULL,
+  `async_script` mediumtext NOT NULL,
+  `sync_delay` int(11) NOT NULL,
+  `async_delay` int(11) NOT NULL,
+  `is_start` tinyint(1) NOT NULL,
+  `space_id` bigint(20) NOT NULL,
+  `last_update_user` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `access_authority` varchar(255) NOT NULL,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `uk_request_mapping` (`request_uri`) USING BTREE,
+  KEY `idx_space_id` (`space_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+ 
+ 
+CREATE TABLE `anymock_tcp_interface_snapshot` (
+  `tcp_interface_id` bigint(20) NOT NULL,
+  `tcp_interface_request_uri` varchar(255) NOT NULL,
+  `tcp_interface_space_id` bigint(20) NOT NULL,
+  `tcp_interface` mediumtext NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `op_type` varchar(255) NOT NULL,
+  `gmt_create` datetime NOT NULL,
+  `gmt_modified` datetime NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_id` (`tcp_interface_id`) USING BTREE,
+  KEY `idx_request_mapping` (`tcp_interface_request_uri`) USING BTREE,
+  KEY `idx_space_id` (`tcp_interface_space_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=290 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
